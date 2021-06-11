60a9cee368180d9c41df2def7121d3ff1c2733ba
Doug Richardson <git@rekt.email>
2021-06-11 12:55:06 -0700

    document config options

--- Plugin/Plugins/DeveloperSettingsExample/Source/DeveloperSettingsExample/Private/MyDeveloperSettings.h
+++ Plugin/Plugins/DeveloperSettingsExample/Source/DeveloperSettingsExample/Private/MyDeveloperSettings.h
@@ -16,6 +16,10 @@
 // Without defaultconfig, changes will be saved in Saved/Config/Windows/XYZ.ini.
 //
 
+// config = Game, but defaultconfig not specified. Changes made in Project
+// Settings will be saved to Saved/Config/Windows/Game.ini.  If you press the
+// "Set Default" button in Project Settings, Config/DefaultGame.ini will be
+// updated.
 UCLASS(config = Game)
 class UMyGameSettings : public UDeveloperSettings
 {
@@ -27,8 +31,13 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
 
+// config = Game and defaultconfig. Changes made in Project Settings will be
+// saved to Config/DefaultGame.ini.
 UCLASS(config = Game, defaultconfig)
 class UMyGameDefaultConfigSettings : public UDeveloperSettings
 {
@@ -40,8 +49,14 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
 
+// config = Engine but no defaultconfig. Changes made in Project Settings will
+// be saved to Saved/Config/Windows/Engine.ini. If you press "Set Default"
+// button in Project Settings, it will update COnfig/DefaultEngine.ini.
 UCLASS(config = Engine)
 class UMyEngineSettings : public UDeveloperSettings
 {
@@ -53,8 +68,13 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
 
+// config = Engine and defaultconfig. Changes made in Project Settings editor
+// will be saved to Config/DefaultEngine.ini.
 UCLASS(config = Engine, defaultconfig)
 class UMyEngineDefaultConfigSettings : public UDeveloperSettings
 {
@@ -66,8 +86,15 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
 
+// config = MyCustom (not one of the default values) and no defaultconfig.
+// Changes made in Project Settings editor will be saved to
+// Saved/Config/Windows/MyCustom.ini. If "Set Default" button is pressed,
+// changes made to Config/DefaultMyCustom.ini.
 UCLASS(config = MyCustom)
 class UMyCustomSettings : public UDeveloperSettings
 {
@@ -79,8 +106,13 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
 
+// config = MyCustom and defaultconfig. Changes made in Project Settings saved
+// to Config/DefaultMyCustom.iini.
 UCLASS(config = MyCustom, defaultconfig)
 class UMyCustomDefaultConfigSettings : public UDeveloperSettings
 {
@@ -92,4 +124,24 @@ public:
 
 	UPROPERTY(Config, EditAnywhere)
 	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
+};
+
+// A bug testing a defaultconfig settings with a custom config.
+UCLASS(config = BugTest, defaultconfig)
+class UBugTestgSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere)
+	int32 SomeInt = 33;
+
+	UPROPERTY(Config, EditAnywhere)
+	float MyFloat = 3.14;
+
+	UPROPERTY(Config, EditAnywhere)
+	FSoftObjectPath MyPath;
 };
