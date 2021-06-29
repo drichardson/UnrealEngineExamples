d7246a3ccecaa05b0d121cd36549240f3112d20b
Doug Richardson <git@rekt.email>
2021-06-29 13:13:01 -0700

    simplify

--- MemoryManagement/Source/MemoryManagement/MemoryManagement.cpp
+++ MemoryManagement/Source/MemoryManagement/MemoryManagement.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "MemoryManagement.h"
+#include "CoreMinimal.h"
+
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MemoryManagement, "MemoryManagement" );
